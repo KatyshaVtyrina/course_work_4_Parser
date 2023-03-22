5519e47d2c74a3667939d3be9488bc2fabcac151
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-03-22 18:19:36 +0300

    correction main

--- main.py
+++ main.py
@@ -1,33 +1,45 @@
 import os
 
-from classes.connector import Connector
-from classes.engine import HH, SuperJob
-from utils.utils import get_top_vacancies_by_date, get_top_vacancies_by_to_salary, print_vacancies
+from classes.engine import Engine, HH, SuperJob
+from utils.utils import get_top_vacancies_by_date, print_info, check_search, \
+    get_top_vacancies_by_to_salary
 
 
 def main():
 
-    path_all = os.path.join('data/all.json')
-    path_no_experience = os.path.join('data/no_experience.json')
+    path = os.path.join('data/all.json')
+    connector = Engine.get_connector(path)
 
+    # поиск вакансии по запросу
     while True:
+
         search = input("Введите слово, по которому будем искать вакансию\n")
-        hh_all = HH(search)
-        hh_no_experience = HH(search, 'noExperience')
-        sj_all = SuperJob(search)
-        if hh_all.get_request()['items'] == [] or sj_all.get_request()['objects'] == []:
+
+        while True:
+            search_experience = input("Будем искать вакансии без опыта работы? Y/N\n")
+            if search_experience.upper() not in ['Y', 'N']:
+                print('Введите Y или N')
+                continue
+            else:
+                break
+
+        if search_experience.upper() == 'Y':
+            hh = HH(search, 'noExperience')
+            sj = SuperJob(search, 'noExperience')
+
+        else:
+            hh = HH(search)
+            sj = SuperJob(search)
+
+        if check_search(hh, sj):
+            all_vacancies = hh.get_vacancies() + sj.get_vacancies()
+            connector.insert(all_vacancies)
+            break
+        else:
             print('Такой вакансии нет')
             continue
-        else:
-            data_all = hh_all.get_vacancies() + sj_all.get_vacancies()
-            all_vacancies = Connector(path_all)
-            all_vacancies.insert(data_all)
 
-            data_no_experience = hh_no_experience.get_vacancies() + sj_all.get_vacancies_no_experience()
-            no_experience_vacancies = Connector(path_no_experience)
-            no_experience_vacancies.insert(data_no_experience)
-            break
-
+    # запрос количества вакансий для вывода
     while True:
         top_count = input('Укажите какое количество вакансий будем выводить на экран\n')
         if not top_count.isdigit() or int(top_count) <= 0:
@@ -37,52 +49,40 @@ def main():
             top_count = int(top_count)
             break
 
+    vacancies = connector.select({})
+
+    # меню
     while True:
-        try:
-            print(f"Меню:\n\
-                1 - вывести {top_count} последних вакансии\n\
-                2 - вывести топ-{top_count} вакансий заработку\n\
-                3 - вывести {top_count} последних вакансий без опыта работы \n\
-                4 - вывести топ-{top_count} вакансий по заработку без опыта работы \n\
-                stop - закончить работу")
-            print()
-            user_input = input("Введите нужный вариант\n")
+        print(f'Меню:\n\
+                        1 - вывести {top_count} последних вакансии\n\
+                        2 - вывести топ-{top_count} вакансий заработку\n\
+                        stop - закончить работу')
+        print()
+        user_input = input("Введите нужный вариант\n")
 
-            if user_input == '1':
-                data = get_top_vacancies_by_date(file=path_all, top_count=top_count)
-                print_vacancies(data)
+        if user_input == '1':
+            data = get_top_vacancies_by_date(data=vacancies, top_count=top_count)
+            print_info(data)
 
-            elif user_input == '2':
-                data = get_top_vacancies_by_to_salary(file=path_all, top_count=top_count)
-                print_vacancies(data)
+        elif user_input == '2':
+            data = get_top_vacancies_by_to_salary(data=vacancies, top_count=top_count)
+            print_info(data)
 
-            elif user_input == '3':
-                data = get_top_vacancies_by_date(file=path_no_experience, top_count=top_count)
-                print_vacancies(data)
+        elif user_input.lower() == 'stop':
+            print('Программа завершает работу')
+            break
 
-            elif user_input == '4':
-                data = get_top_vacancies_by_to_salary(file=path_no_experience, top_count=top_count)
-                print_vacancies(data)
-
-            elif user_input.lower() == 'stop':
-                print('Программа завершает работу')
-                break
-
-            else:
-                raise ValueError
-
-        except ValueError:
+        else:
             print("Такого варианта нет, попробуйте еще раз")
             continue
 
+        print('Показать еще меню? Y/N')
+        choice = input().upper()
+        if choice == 'Y':
+            continue
         else:
-            print('Показать еще меню? Y/N')
-            choice = input().upper()
-            if choice == 'Y':
-                continue
-            else:
-                print('Программа завершает работу')
-                break
+            print('Программа завершает работу')
+            break
 
     exit()
 
